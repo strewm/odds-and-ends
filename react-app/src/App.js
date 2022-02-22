2a66f45bcf6018f3ce05d1cb05a0f9f631475ae7
Savanah Trewman <strewman@me.com>
2022-02-21 20:24:18 -0600

    added moving navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -28,6 +28,8 @@ function App() {
     return null;
   }
 
+
+
   return (
     <BrowserRouter>
       <NavBar />
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../Auth/LogoutButton';
 import CreatePostingModal from '../Postings/CreatePostingModal';
@@ -6,37 +6,63 @@ import CreatePostingModal from '../Postings/CreatePostingModal';
 import './Navigation.css';
 
 const NavBar = () => {
+  const [width, setWidth] = useState('0%');
+
+  const openNav = () => {
+    setWidth('25%');
+  };
+
+  const closeNav = () => {
+    setWidth('0%');
+  };
+
   return (
-    <nav className='nav-container'>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <CreatePostingModal />
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <>
+      <button onClick={openNav}>Open</button>
+      <div className='nav-container' id='mySidenav' style={{ width: width }}>
+        <button onClick={closeNav}>Close</button>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+        <NavLink to='/login' exact={true} activeClassName='active'>
+          Login
+        </NavLink>
+        <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          Sign Up
+        </NavLink>
+        <NavLink to='/users' exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+        {/* <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <CreatePostingModal />
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul> */}
+      </div>
+    </>
   );
 }
 
