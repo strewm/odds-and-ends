9014b7b56a0a73b737360e131e9e4d11b62acda7
Savanah Trewman <strewman@me.com>
2022-02-23 11:12:12 -0600

    added more validations to postings

--- app/forms/posting_form.py
+++ app/forms/posting_form.py
@@ -18,8 +18,8 @@ def check_zip_number(form, field):
 
 
 class PostingForm(FlaskForm):
-    address = StringField('address', validators=[DataRequired()])
-    city = StringField('city', validators=[DataRequired()])
+    address = StringField('address', validators=[DataRequired(), Length(min=5, max=100, message='Address must be 5-100 characters.')])
+    city = StringField('city', validators=[DataRequired(), Length(min=3, max=100, message='City must be 3-100 characters.')])
     state = StringField('state', validators=[DataRequired(), Length(min=2, max=2, message='State must be 2 characters.')])
     zipcode = StringField('zipcode', validators=[DataRequired(), check_zip_number, Length(min=5, max=5, message='Zipcode must be 5 characters.')])
     title = StringField('title', validators=[DataRequired(), Length(min=3, max=30, message='Title must be 3-30 characters.')])
