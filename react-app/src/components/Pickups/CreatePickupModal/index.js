e31338fb77b89e01b0d24ca6cddf3f2c0adfe73e
Savanah Trewman <strewman@me.com>
2022-02-24 13:53:12 -0600

    more single posting styling

--- react-app/src/components/Pickups/CreatePickupModal/index.js
+++ react-app/src/components/Pickups/CreatePickupModal/index.js
@@ -11,7 +11,7 @@ function CreatePickupModal({ posting, setUpdate }) {
   return (
     <>
       {/* <i onClick={() => setShowModal(true)} className="fa-solid fa-circle-plus"></i> */}
-      <div id='create-pickup' onClick={() => setShowModal(true)}>Schedule Pickup</div>
+      <div id='create-pickup' onClick={() => setShowModal(true)}>S C H E D U L E &nbsp;&nbsp; P I C K U P</div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <CreatePickup setShowModal={setShowModal} setUpdate={setUpdate} posting={posting} />
