79327b5f033d8cd86d96b0b9c9f3af04da34c8e7
Savanah Trewman <strewman@me.com>
2022-02-27 16:26:53 -0600

    working on maps embed BUT issue with maps not accepting api key

--- react-app/src/components/MapsAPI/index.js
+++ react-app/src/components/MapsAPI/index.js
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom'
 
 // import './Maps.css'
 
-let key = process.env.MAPS_API_KEY
+const environment = process.env.REACT_APP_MAPS_API_KEY
+// console.log(key)
 
 const Maps = () => {
     const { postingId } = useParams()
@@ -11,28 +12,38 @@ const Maps = () => {
 
     console.log('-----------posting', posting)
 
-    let address;
-    let city;
-    let state;
-    let zipcode;
 
-    const loadMap = async () => {
-        address = posting[address];
-        city = posting[city];
-        state = posting[state];
-        zipcode = posting[zipcode];
-    }
+    // let address;
+    // let city;
+    // let state;
+    // let zipcode;
+
+
+    // const loadMap = async () => {
+    //     address = posting['address'];
+    //     city = posting['city'];
+    //     state = posting['state'];
+    //     zipcode = posting['zipcode'];
+    // }
+    // console.log(address, city, state, zipcode)
+
+    console.log(posting?.address)
+
+    const address = posting?.address;
+    const city = posting?.city;
+    const state = posting?.state;
+    const zipcode = posting?.zipcode;
 
     // const singleMap = businesses.find(single => single.id === +id)
 
     return (
-        <div onLoad={loadMap}>
-            <div>hi</div>
+            // <div>hi</div>
+        <div >
             <iframe
                 className='google-map'
                 title='posting-map'
-                src={`https://www.google.com/maps/embed/v1/place?key=${key}
-                &q=${address},${city}+${state}+${zipcode}`}>
+                src={`https://www.google.com/maps/embed/v1/place?key=${environment}
+                &q=${address},${city}+${state}`}>
             </iframe>
         </div>
     )
