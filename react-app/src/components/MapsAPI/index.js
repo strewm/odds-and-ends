163b2c397e2fd51f8033f17e1198527fa363cb11
Savanah Trewman <strewman@me.com>
2022-03-01 17:26:59 -0600

    added loading spinner to maps

--- react-app/src/components/MapsAPI/index.js
+++ react-app/src/components/MapsAPI/index.js
@@ -1,15 +1,15 @@
-import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import './Maps.css'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import './Maps.css';
 
 const API_KEY = process.env.REACT_APP_MAPS_API_KEY
 
 const Maps = () => {
-    const { postingId } = useParams()
-    const posting = useSelector(state => state.postings[postingId])
+    const [loading, setLoading] = useState(true);
 
-    // console.log('-----------posting', posting)
-    // console.log(posting?.address)
+    const { postingId } = useParams();
+    const posting = useSelector(state => state.postings[postingId]);
 
     const address = posting?.address;
     const city = posting?.city;
@@ -18,14 +18,24 @@ const Maps = () => {
 
     // const postingMap = posting.find(post => post.id === +postingId)
 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
     return (
-        <div >
-            <iframe
-                className='google-map'
-                title='posting-map'
-                src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}
-                &q=${address},${city}+${state}+${zipcode}`}>
-            </iframe>
+        <div>
+            {loading ?
+                <div className='loader-container'><div id='loader'></div></div>
+                :
+                <iframe
+                    className='google-map'
+                    title='posting-map'
+                    src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}
+                    &q=${address},${city}+${state}+${zipcode}`}>
+                </iframe>
+            }
         </div>
     )
 }
