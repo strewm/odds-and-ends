7a1604eb42dbb8b3f98458dbde4e9735501a74ad
Savanah Trewman <strewman@me.com>
2022-03-11 13:35:18 -0600

    working on search from pages other than / bug

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,14 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink, useParams, useHistory } from 'react-router-dom';
 import { getAllPostings } from '../../store/postings';
 
 import './Search.css'
 
-function Search() {
+const Search = () => {
     const [search, setSearch] = useState('');
     const [results, setResults] = useState([]);
     const dispatch = useDispatch();
+
+    // const history = useHistory();
+    // const historyCheck = history.location.pathname;
+
+    // console.log('historyyyyy',history.location.pathname)
+
+    // let path = '';
+
+    // const handleClick = () => {
+    //     // history.push(`postings/`);
+
+    //     // Three history options = /, /postings/#, /username
+    //     if (historyCheck.length === 1) {
+    //         path = 'postings/'
+    //     } else if (/\/postings\/.*/.test(historyCheck)) {
+    //         console.log('it works')
+    //         path = '/'
+    //     } else {
+    //         path = 'postings/'
+    //     }
+    // }
 
 
     useEffect(async () => {
@@ -34,10 +55,18 @@ function Search() {
     let resultCheck;
     if (search && results.length >= 1) {
         resultCheck = results.map(result => (
-            <NavLink className='search-result' to={`postings/${result.id}`} onClick={() => setResults('')} key={result.id}>
+            <NavLink className='search-result' onClick={() => {setResults('');}} to={`postings/${result.id}`} key={result.id}>
                 <div id='result-title'>{result.title}</div>
                 <div id='result-location'>{result.city}, {result.state}</div>
             </NavLink>
+            // <Link className='search-result' to={`postings/${result.id}`} onClick={() => setResults('')} key={result.id}>
+            //     <div id='result-title'>{result.title}</div>
+            //     <div id='result-location'>{result.city}, {result.state}</div>
+            // </Link>
+            // <div className='search-result' onClick={() => {handleClick(); setResults('');}} key={result.id}>
+            //     <div id='result-title'>{result.title}</div>
+            //     <div id='result-location'>{result.city}, {result.state}</div>
+            // </div>
         ))
     } else if (search && results.length === 0) {
         resultCheck = <div className="search-result">Nothing here! Try a different search...</div>
