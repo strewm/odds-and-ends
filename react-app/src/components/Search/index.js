f89ab8b4cd855185ffffb2176e421ac6e227f948
Savanah Trewman <strewman@me.com>
2022-03-11 15:13:08 -0600

    fixed search bug, NavLinks needed a '/' to start to path.......

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -10,28 +10,6 @@ const Search = () => {
     const [results, setResults] = useState([]);
     const dispatch = useDispatch();
 
-    // const history = useHistory();
-    // const historyCheck = history.location.pathname;
-
-    // console.log('historyyyyy',history.location.pathname)
-
-    // let path = '';
-
-    // const handleClick = () => {
-    //     // history.push(`postings/`);
-
-    //     // Three history options = /, /postings/#, /username
-    //     if (historyCheck.length === 1) {
-    //         path = 'postings/'
-    //     } else if (/\/postings\/.*/.test(historyCheck)) {
-    //         console.log('it works')
-    //         path = '/'
-    //     } else {
-    //         path = 'postings/'
-    //     }
-    // }
-
-
     useEffect(async () => {
         const results = [];
 
@@ -55,18 +33,10 @@ const Search = () => {
     let resultCheck;
     if (search && results.length >= 1) {
         resultCheck = results.map(result => (
-            <NavLink className='search-result' onClick={() => {setResults('');}} to={`postings/${result.id}`} key={result.id}>
+            <NavLink className='search-result' onClick={() => setResults('')} to={`/postings/${result.id}`} key={result.id}>
                 <div id='result-title'>{result.title}</div>
                 <div id='result-location'>{result.city}, {result.state}</div>
             </NavLink>
-            // <Link className='search-result' to={`postings/${result.id}`} onClick={() => setResults('')} key={result.id}>
-            //     <div id='result-title'>{result.title}</div>
-            //     <div id='result-location'>{result.city}, {result.state}</div>
-            // </Link>
-            // <div className='search-result' onClick={() => {handleClick(); setResults('');}} key={result.id}>
-            //     <div id='result-title'>{result.title}</div>
-            //     <div id='result-location'>{result.city}, {result.state}</div>
-            // </div>
         ))
     } else if (search && results.length === 0) {
         resultCheck = <div className="search-result">Nothing here! Try a different search...</div>
